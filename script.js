171bf2ef8c4f9471179842e410cc4cc62a89df32
Miwhans <hilmyraihan300@gmail.com>
2026-04-07 17:57:06 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let dataPasien = JSON.parse(localStorage.getItem("pasien")) || [];
+
+function tampilData() {
+  let tabel = document.getElementById("tabel");
+  tabel.innerHTML = "";
+
+  dataPasien.forEach((item, index) => {
+    tabel.innerHTML += `
+      <tr>
+        <td>${item.nama}</td>
+        <td>${item.umur}</td>
+        <td>${item.diagnosa}</td>
+        <td>
+          <button onclick="editData(${index})">Edit</button>
+          <button onclick="hapusData(${index})">Hapus</button>
+        </td>
+      </tr>
+    `;
+  });
+
+  localStorage.setItem("pasien", JSON.stringify(dataPasien));
+}
+
+function tambahData() {
+  let nama = document.getElementById("nama").value;
+  let umur = document.getElementById("umur").value;
+  let diagnosa = document.getElementById("diagnosa").value;
+
+  if (nama === "" || umur === "" || diagnosa === "") {
+    alert("Semua field harus diisi!");
+    return;
+  }
+
+  dataPasien.push({ nama, umur, diagnosa });
+  tampilData();
+}
+
+function hapusData(index) {
+  dataPasien.splice(index, 1);
+  tampilData();
+}
+
+function editData(index) {
+  let data = dataPasien[index];
+  document.getElementById("nama").value = data.nama;
+  document.getElementById("umur").value = data.umur;
+  document.getElementById("diagnosa").value = data.diagnosa;
+
+  hapusData(index);
+}
+
+tampilData();
